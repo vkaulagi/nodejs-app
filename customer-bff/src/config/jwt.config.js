6fabffd030a6aa9bfd52f700ac0b4bbe51da1ed2
vkaulagi <vskaulagi@gmail.com>
2023-08-11 22:08:47 -0400

    first commit

--- customer-bff/src/config/jwt.config.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const JWT_SECRET = process.env.JWT_SECRET || 'my-secret-key';
-console.log('JWT Secret Key:', JWT_SECRET);
-
-export default JWT_SECRET;
